d732c061e8968a0d9e15a721d1c2994bc26ab2c2
BlanceXR <yizhenwei09@gmail.com>
2015-09-19 00:46:06 -0400

    added city autocomplete

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
 const Search = React.createClass({
-  render : function() {
-    return (
-      <h1>Search</h1>
-    );
-  }
+
+  componentDidMount: function() {
+       var inputOptions = {types: ['(cities)']};
+       new google.maps.places.Autocomplete(
+           document.getElementById('searchTextField'),
+           inputOptions);
+
+  },
+  
+   buttonClick: function() {
+       alert(this.refs.searchField.getDOMNode().value);
+   },
+
+  
+   render: function() {
+       return (
+           <div>
+               <label htmlFor="searchTextField">
+               Please Insert an address:
+               </label>
+               <br/>
+               <input ref='searchField' id="searchTextField" type="text" size="50"/>
+               <br/>
+               <button onClick={this.buttonClick}>Submit</button>
+               <br />
+           </div>
+           );
+   }
+  
 });
 
 export default Search;
