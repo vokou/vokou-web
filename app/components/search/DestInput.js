110bf7f2615ac6d2af08c9cfe2de9d8672c43553
LASkuma <jimmyx@me.com>
2015-10-11 17:36:42 -0400

    Autocomplete fix

--- app/components/search/DestInput.js
+++ app/components/search/DestInput.js
@@ -64,7 +64,7 @@ const DestInput = React.createClass({
           defaultValue={this.props.defaultValue}
           id="destination"
           onChange={this.handleInputChange}
-          //onBlur={this.handleBlur}
+          onBlur={this.handleBlur}
           onFocus={this.handleFocus}
           underlineFocusStyle={{borderColor: '#d3d3d3'}}/>
         <CompleteMenu
