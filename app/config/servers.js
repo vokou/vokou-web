3093330f0ab1e5a225384a20a1a068caf068c6e6
LASkuma <jimmyx@me.com>
2015-10-21 19:18:30 -0400

    Production setting

--- app/config/servers.js
+++ app/config/servers.js
@@ -1,5 +1,5 @@
 export default {
-  api: 'http://52.27.219.93:8888',
-  proxy: 'http://52.26.153.30:8080',
+  api: 'http://api.vokou.com:8888',
+  proxy: 'http://cors-balancer-1481724427.us-west-2.elb.amazonaws.com',
   vokou: 'https://vokou.parseapp.com/'
 }
