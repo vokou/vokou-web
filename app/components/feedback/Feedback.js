7028994e219481f45fd870308df69b7db8ff1a72
LASkuma <jimmyx@me.com>
2015-10-22 14:03:25 -0400

    try to fix feedback

--- app/components/feedback/Feedback.js
+++ app/components/feedback/Feedback.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import $ from 'jquery';
 import feedbackLib from '../../libs/feedback.min.js';
 import feedbackstyle from '../../libs/feedback.min.css';
-import $ from 'jquery';
 
 const Feedback = React.createClass({
-  
+  componentDidMount() {
+    $.feedback({
+      ajaxURL: 'https://vokou.parseapp.com/feedback',
+      html2canvasURL: 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js'
+    });
+  },
   render() {
     return (
       <div>
