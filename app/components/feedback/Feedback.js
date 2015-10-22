3a368108d87ccce7d719707f12676d30e948fc9d
BlanceXR <yizhenwei09@gmail.com>
2015-10-21 23:06:34 -0400

    feedback need ajax post url. In index.html

---
+++ app/components/feedback/Feedback.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import feedbackLib from '../../libs/feedback.min.js';
+import feedbackstyle from '../../libs/feedback.min.css';
+import $ from 'jquery';
+
+const Feedback = React.createClass({
+  
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+});
+
+export default Feedback;
