e249bced34e53439449389119c22a1d073fc7532
LASkuma <jimmyx@me.com>
2015-10-24 01:11:44 -0400

    production settings

--- app/config/servers.js
+++ app/config/servers.js
@@ -1,5 +1,5 @@
 export default {
-  api: 'http://52.27.219.93:8888',
-  proxy: 'http://52.26.153.30:8080',
-  vokou: 'https://vokou.parseapp.com/'
+  api: 'http://apis.vokou.com',
+  proxy: 'http://redirect.vokou.com',
+  vokou: 'https://vokou-web.parseapp.com/'
 }
