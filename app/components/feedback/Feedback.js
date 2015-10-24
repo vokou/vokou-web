b151f32eb2e3ce6447f1c326e4f155658d692939
LASkuma <jimmyx@me.com>
2015-10-24 02:29:05 -0400

    Change feedback url

--- app/components/feedback/Feedback.js
+++ app/components/feedback/Feedback.js
@@ -2,11 +2,12 @@ import React from 'react';
 import $ from 'jquery';
 import feedbackLib from '../../libs/feedback.min.js';
 import feedbackstyle from '../../libs/feedback.min.css';
+import servers from '../../config/servers';
 
 const Feedback = React.createClass({
   componentDidMount() {
     $.feedback({
-      ajaxURL: 'https://vokou.parseapp.com/feedback',
+      ajaxURL: `${servers.vokou}feedback`,
       html2canvasURL: 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js'
     });
   },
