f536df652ffda96cd089a476cc812c04ce1fffda
BlanceXR <yizhenwei09@gmail.com>
2015-10-23 22:23:31 -0400

    remove semicoloum

--- app/components/detail/DetailPage.js
+++ app/components/detail/DetailPage.js
@@ -139,7 +139,7 @@ var DetailPage = React.createClass({
                 src={this.state.thumbnail[3]}
                 onClick={this.openGallery.bind(this, 3)}
                 className="sq-img"/>
-            </div>;
+            </div>
             
             <div className="col-md-5">
               <h3 className="detail-text">
